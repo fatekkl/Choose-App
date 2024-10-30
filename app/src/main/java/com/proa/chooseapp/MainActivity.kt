60458058ec9d9e9a9e82bb104106d8764c93ddd9
fatekkl <mathtml.1105@gmail.com>
2024-10-30 18:41:02 -0300

    change to add images

--- app/src/main/java/com/proa/chooseapp/MainActivity.kt
+++ app/src/main/java/com/proa/chooseapp/MainActivity.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -18,8 +23,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.proa.chooseapp.ui.theme.ChooseAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,29 +42,55 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize()
                 ) {
                     var random by remember {
-                        mutableIntStateOf(0)
+                        mutableIntStateOf(1)
                     }
+
+                    val choose = when (random) {
+                        1 -> "Arroz e Feijão"
+                        2 -> "Batata Frita"
+                        3 -> "Sushi"
+                        4 -> "Pizza"
+                        5 -> "Hamburguer"
+                        else -> "Arroz e Feijão"
+                    }
+
+                    val image = when (random) {
+                        1 -> R.drawable.arroz_feijao
+                        2 -> R.drawable.batata_frita
+                        3 -> R.drawable.sushi
+                        4 -> R.drawable.pizza
+                        5 -> R.drawable.hamburguer
+                        else -> R.drawable.arroz_feijao
+                    }
+
 
                     Column(
                         verticalArrangement = Arrangement.Center,
                         horizontalAlignment = Alignment.CenterHorizontally,
-                        modifier = Modifier.fillMaxWidth()
                     ) {
-                        Text("O que você vai comer?", color = Color.White)
-                        Button(onClick = { random = (0..5).random() }) {
-                            Text("Sortear")
-                        }
+                        Text("O que você vai comer?", color = Color.White, fontSize = 30.sp)
+
+
                         Text(
-                            text = when (random) {
-                                1 -> "Arroz e Feijão"
-                                2 -> "Feijoada"
-                                3 -> "Sushi"
-                                4 -> "Pizza"
-                                5 -> "Hamburguer"
-                                else -> "Comida"
-                            },
-                            color = Color.White
+                            text = choose,
+                            color = Color.White,
+                            fontSize = 30.sp,
+                            modifier = Modifier.padding(top = 80.dp)
                         )
+
+                        Image(
+                            painter = painterResource(image),
+                            contentDescription = "Filme",
+                            modifier = Modifier
+                                .width(225.dp).height(250.dp)
+                                .padding(bottom = 8.dp).clip(shape = RoundedCornerShape(10.dp))
+                        )
+
+                        Button(onClick = { random = (1..5).random() }, modifier = Modifier.size(width = 200.dp, height = 50.dp)) {
+                            Text("Sortear", fontSize = 25.sp)
+                        }
+
+
 
                     }
                 }
